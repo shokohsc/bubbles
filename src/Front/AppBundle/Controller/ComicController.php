e273f8153d317a1e16c5a0c637e7d278953b0e23
dimitridisko <dimitri@disko.fr>
2015-04-05 11:02:19 +0200

    removed unnecessary use statements

--- src/Front/AppBundle/Controller/ComicController.php
+++ src/Front/AppBundle/Controller/ComicController.php
@@ -4,9 +4,6 @@ namespace Front\AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Carbon\Carbon;
 
 class ComicController extends Controller
 {
--- src/Front/AppBundle/Controller/CreatorController.php
+++ src/Front/AppBundle/Controller/CreatorController.php
@@ -4,9 +4,7 @@ namespace Front\AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Carbon\Carbon;
 
 class CreatorController extends Controller
 {
