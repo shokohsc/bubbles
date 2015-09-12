354b5ee84665083c145637068b5c213b9a71b5ff
shokohsc <shokohsc@gmail.com>
2015-09-12 22:29:22 +0200

    double quotes to simple ones

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     var height = $('footer').height();
     $('body').css({
-        "margin-bottom": height
+        'margin-bottom': height
     });
 
     $('.search-panel .dropdown-menu').find('a').click(function(e) {
         e.preventDefault();
-        var param = $(this).attr("href").replace("#","");
+        var param = $(this).attr('href').replace('#','');
         var concept = $(this).text();
         $('.search-panel span#search_concept').text(concept);
         $('#entity').val(param.toLowerCase());
