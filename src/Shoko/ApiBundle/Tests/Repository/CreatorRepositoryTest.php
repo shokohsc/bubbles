0cd43bbdf8569a1941c79947136b3eb05826da86
shokohsc <shokohsc@gmail.com>
2015-11-02 22:18:08 +0100

    updated creator repository test to use prophecy

--- src/Shoko/ApiBundle/Tests/Repository/CreatorRepositoryTest.php
+++ src/Shoko/ApiBundle/Tests/Repository/CreatorRepositoryTest.php
@@ -6,78 +6,118 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Shoko\ApiBundle\Repository\CreatorRepository;
 use Octante\MarvelAPIBundle\Repositories\CreatorsRepository;
 use Octante\MarvelAPIBundle\Repositories\ComicsRepository;
+use Prophecy\Prophet;
+use Prophecy\Argument;
 
+/**
+ * CreatorRepositoryTest class.
+ */
 class CreatorRepositoryTest extends \PHPUnit_Framework_TestCase
 {
+      /**
+       * Prophet $prophet.
+       *
+       * @var Prophet
+       */
+      private $prophet;
+
+      /**
+       * Octante\MarvelAPIBundle\Lib\Client $client.
+       *
+       * @var Octante\MarvelAPIBundle\Lib\Client
+       */
       private $client;
+
+      /**
+       * Octante\MarvelAPIBundle\Model\Query\CreatorQuery $queryMock.
+       *
+       * @var Octante\MarvelAPIBundle\Model\Query\CreatorQuery
+       */
+      private $queryMock;
+
+      /**
+       * Octante\MarvelAPIBundle\Repositories\ComicsRepository $comicRepositoryMock.
+       *
+       * @var Octante\MarvelAPIBundle\Repositories\ComicsRepository
+       */
       private $comicRepositoryMock;
+
+      /**
+      * Octante\MarvelAPIBundle\Model\Query\ComicQuery $comicQueryMock.
+      *
+      * @var Octante\MarvelAPIBundle\Model\Query\ComicQuery
+       */
       private $comicQueryMock;
+
+      /**
+       * int $comicsPerPage.
+       *
+       * @var int
+       */
       private $comicsPerPage;
 
+      /**
+       * {@inheritdoc}
+       */
       public function setUp()
       {
-        $this->client = $this->getMockBuilder('Octante\MarvelAPIBundle\Lib\Client')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->queryMock = $this->getMockBuilder('Octante\MarvelAPIBundle\Model\Query\CreatorQuery')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->comicRepositoryMock = $this->getMockBuilder('Octante\MarvelAPIBundle\Repositories\ComicsRepository')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->comicQueryMock = $this->getMockBuilder('Octante\MarvelAPIBundle\Model\Query\ComicQuery')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->prophet = new Prophet();
+        $this->client = $this->prophet->prophesize('Octante\MarvelAPIBundle\Lib\Client');
+        $this->queryMock = $this->prophet->prophesize('Octante\MarvelAPIBundle\Model\Query\CreatorQuery');
+        $this->comicRepositoryMock = $this->prophet->prophesize('Octante\MarvelAPIBundle\Repositories\ComicsRepository');
+        $this->comicQueryMock = $this->prophet->prophesize('Octante\MarvelAPIBundle\Model\Query\ComicQuery');
         $this->comicsPerPage = 20;
       }
 
+      /**
+       * testFindAllComicsById method
+       */
       public function testFindAllComicsById()
       {
         $jsonResponse = file_get_contents(__DIR__ . '/../Fixtures/Creator/getCreatorComics.json');
-        $this->client
-            ->expects($this->any())
-            ->method('send')
-            ->will($this->returnValue($jsonResponse));
-        $sut = new CreatorsRepository($this->client);
-        $stubComics = $sut->getComicsFromCreator(12339, $this->queryMock);// Kevin P. Wada
-
-        $sutComicsRepository = new ComicsRepository($this->client);
-        $repository = new CreatorRepository($sut, $this->queryMock, $sutComicsRepository, $this->comicQueryMock, $this->comicsPerPage);
+        $this->client->send(Argument::any())->willReturn($jsonResponse);
+        $sut = new CreatorsRepository($this->client->reveal());
+        $stubComics = $sut->getComicsFromCreator(12339, $this->queryMock->reveal());// Kevin P. Wada
+        $sutComicsRepository = new ComicsRepository($this->client->reveal());
+        $repository = new CreatorRepository($sut, $this->queryMock->reveal(), $sutComicsRepository, $this->comicQueryMock->reveal(), $this->comicsPerPage);
         $comics = $repository->findAllComicsById(12339, 1);
-        $this->assertEquals($stubComics->getData()->getResults(), $comics);
+        $this->assertEquals($stubComics->getData()->getResults(), $comics->getResults());
       }
 
+      /**
+       * testFindOneById method
+       */
       public function testFindOneById()
       {
         $jsonResponse = file_get_contents(__DIR__ . '/../Fixtures/Creator/getCreator.json');
-        $this->client
-            ->expects($this->any())
-            ->method('send')
-            ->will($this->returnValue($jsonResponse));
-
-        $sut = new CreatorsRepository($this->client);
+        $this->client->send(Argument::any())->willReturn($jsonResponse);
+        $sut = new CreatorsRepository($this->client->reveal());
         $stubCreator = $sut->getCreatorById(2997);// Fred Hembeck
-
-        $repository = new CreatorRepository($sut, $this->queryMock, $this->comicRepositoryMock, $this->comicQueryMock, $this->comicsPerPage);
+        $repository = new CreatorRepository($sut, $this->queryMock->reveal(), $this->comicRepositoryMock->reveal(), $this->comicQueryMock->reveal(), $this->comicsPerPage);
         $creator = $repository->findOneById(2997);
-        $this->assertInstanceOf('Octante\MarvelAPIBundle\Model\Entities\Creator', $creator);
-        $this->assertEquals($stubCreator->getData()->getResults()[0], $creator);
+        $this->assertInstanceOf('Octante\MarvelAPIBundle\Model\DataContainer\DataContainer', $creator);
+        $this->assertEquals($stubCreator->getData()->getResults()[0], $creator->getResults()[0]);
       }
 
+      /**
+       * testFindAllByQuery method
+       */
       public function testFindAllByQuery()
       {
         $jsonResponse = file_get_contents(__DIR__ . '/../Fixtures/Creator/getCreators.json');
-        $this->client
-            ->expects($this->any())
-            ->method('send')
-            ->will($this->returnValue($jsonResponse));
-
-        $sut = new CreatorsRepository($this->client);
-        $stubCreators = $sut->getCreators($this->queryMock);// stan
-
-        $repository = new CreatorRepository($sut, $this->queryMock, $this->comicRepositoryMock, $this->comicQueryMock, $this->comicsPerPage);
-        $creators = $repository->findAllByQuery('stan', 1);
-        $this->assertEquals($stubCreators->getData()->getResults(), $creators);
+        $this->client->send(Argument::any())->willReturn($jsonResponse);
+        $sut = new CreatorsRepository($this->client->reveal());
+        $stubCreators = $sut->getCreators($this->queryMock->reveal());// stan
+        $repository = new CreatorRepository($sut, $this->queryMock->reveal(), $this->comicRepositoryMock->reveal(), $this->comicQueryMock->reveal(), $this->comicsPerPage);
+        $creators = $repository->findAllByQuery('spider', 1);
+        $this->assertEquals($stubCreators->getData()->getResults(), $creators->getResults());
       }
 
+      /**
+       * {@inheritdoc}
+       */
+      public function tearDown()
+      {
+        $this->prophet->checkPredictions();
+      }
 }
