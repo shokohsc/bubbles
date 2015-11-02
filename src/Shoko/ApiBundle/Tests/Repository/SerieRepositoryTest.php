4d13229d8327046437b65e2d3cf8b400c1a8c89c
shokohsc <shokohsc@gmail.com>
2015-11-02 23:14:39 +0100

    updated serie repository test to use prophecy

--- src/Shoko/ApiBundle/Tests/Repository/SerieRepositoryTest.php
+++ src/Shoko/ApiBundle/Tests/Repository/SerieRepositoryTest.php
@@ -6,78 +6,118 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Shoko\ApiBundle\Repository\SerieRepository;
 use Octante\MarvelAPIBundle\Repositories\SeriesRepository;
 use Octante\MarvelAPIBundle\Repositories\ComicsRepository;
+use Prophecy\Prophet;
+use Prophecy\Argument;
 
+/**
+ * SerieRepositoryTest class.
+ */
 class SerieRepositoryTest extends \PHPUnit_Framework_TestCase
 {
+      /**
+       * Prophet $prophet.
+       *
+       * @var Prophet
+       */
+      private $prophet;
+
+      /**
+       * Octante\MarvelAPIBundle\Lib\Client $client.
+       *
+       * @var Octante\MarvelAPIBundle\Lib\Client
+       */
       private $client;
+
+      /**
+       * Octante\MarvelAPIBundle\Model\Query\SeriesQuery $queryMock.
+       *
+       * @var Octante\MarvelAPIBundle\Model\Query\SeriesQuery
+       */
+      private $queryMock;
+
+      /**
+       * Octante\MarvelAPIBundle\Repositories\ComicsRepository $comicRepositoryMock.
+       *
+       * @var Octante\MarvelAPIBundle\Repositories\ComicsRepository
+       */
       private $comicRepositoryMock;
+
+      /**
+      * Octante\MarvelAPIBundle\Model\Query\ComicQuery $comicQueryMock.
+      *
+      * @var Octante\MarvelAPIBundle\Model\Query\ComicQuery
+       */
       private $comicQueryMock;
+
+      /**
+       * int $comicsPerPage.
+       *
+       * @var int
+       */
       private $comicsPerPage;
 
+      /**
+       * {@inheritdoc}
+       */
       public function setUp()
       {
-        $this->client = $this->getMockBuilder('Octante\MarvelAPIBundle\Lib\Client')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->queryMock = $this->getMockBuilder('Octante\MarvelAPIBundle\Model\Query\SerieQuery')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->comicRepositoryMock = $this->getMockBuilder('Octante\MarvelAPIBundle\Repositories\ComicsRepository')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->comicQueryMock = $this->getMockBuilder('Octante\MarvelAPIBundle\Model\Query\ComicQuery')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->prophet = new Prophet();
+        $this->client = $this->prophet->prophesize('Octante\MarvelAPIBundle\Lib\Client');
+        $this->queryMock = $this->prophet->prophesize('Octante\MarvelAPIBundle\Model\Query\SerieQuery');
+        $this->comicRepositoryMock = $this->prophet->prophesize('Octante\MarvelAPIBundle\Repositories\ComicsRepository');
+        $this->comicQueryMock = $this->prophet->prophesize('Octante\MarvelAPIBundle\Model\Query\ComicQuery');
         $this->comicsPerPage = 20;
       }
 
+      /**
+       * testFindAllComicsById method
+       */
       public function testFindAllComicsById()
       {
         $jsonResponse = file_get_contents(__DIR__ . '/../Fixtures/Serie/getSerieComics.json');
-        $this->client
-            ->expects($this->any())
-            ->method('send')
-            ->will($this->returnValue($jsonResponse));
-        $sut = new SeriesRepository($this->client);
-        $stubComics = $sut->getComicsFromSerie(17959, $this->queryMock);// The Superior Foes of Spider-Man (2013 - Present)
-
-        $sutComicsRepository = new ComicsRepository($this->client);
-        $repository = new SerieRepository($sut, $this->queryMock, $sutComicsRepository, $this->comicQueryMock, $this->comicsPerPage);
-        $comics = $repository->findAllComicsById(12339, 1)->getResults();
-        $this->assertEquals($stubComics->getData()->getResults(), $comics);
+        $this->client->send(Argument::any())->willReturn($jsonResponse);
+        $sut = new SeriesRepository($this->client->reveal());
+        $stubComics = $sut->getComicsFromSerie(17959, $this->queryMock->reveal());// The Superior Foes of Spider-Man (2013 - Present)
+        $sutComicsRepository = new ComicsRepository($this->client->reveal());
+        $repository = new SerieRepository($sut, $this->queryMock->reveal(), $sutComicsRepository, $this->comicQueryMock->reveal(), $this->comicsPerPage);
+        $comics = $repository->findAllComicsById(17959, 1);
+        $this->assertEquals($stubComics->getData()->getResults(), $comics->getResults());
       }
 
+      /**
+       * testFindOneById method
+       */
       public function testFindOneById()
       {
         $jsonResponse = file_get_contents(__DIR__ . '/../Fixtures/Serie/getSerie.json');
-        $this->client
-            ->expects($this->any())
-            ->method('send')
-            ->will($this->returnValue($jsonResponse));
-
-        $sut = new SeriesRepository($this->client);
-        $stubSerie = $sut->getSerieById(18468);// Ms. Marvel (2014 - Present)
-
-        $repository = new SerieRepository($sut, $this->queryMock, $this->comicRepositoryMock, $this->comicQueryMock, $this->comicsPerPage);
-        $serie = $repository->findOneById(18468)->getResults()[0];
-        $this->assertInstanceOf('Octante\MarvelAPIBundle\Model\Entities\Serie', $serie);
-        $this->assertEquals($stubSerie->getData()->getResults()[0], $serie);
+        $this->client->send(Argument::any())->willReturn($jsonResponse);
+        $sut = new SeriesRepository($this->client->reveal());
+        $stubSeries = $sut->getSerieById(18468);// Ms. Marvel (2014 - Present)
+        $repository = new SerieRepository($sut, $this->queryMock->reveal(), $this->comicRepositoryMock->reveal(), $this->comicQueryMock->reveal(), $this->comicsPerPage);
+        $series = $repository->findOneById(18468);
+        $this->assertInstanceOf('Octante\MarvelAPIBundle\Model\DataContainer\DataContainer', $series);
+        $this->assertEquals($stubSeries->getData()->getResults()[0], $series->getResults()[0]);
       }
 
+      /**
+       * testFindAllByQuery method
+       */
       public function testFindAllByQuery()
       {
         $jsonResponse = file_get_contents(__DIR__ . '/../Fixtures/Serie/getSeries.json');
-        $this->client
-            ->expects($this->any())
-            ->method('send')
-            ->will($this->returnValue($jsonResponse));
-
-        $sut = new SeriesRepository($this->client);
-        $stubSeries = $sut->getSeries($this->queryMock);// avengers
-
-        $repository = new SerieRepository($sut, $this->queryMock, $this->comicRepositoryMock, $this->comicQueryMock, $this->comicsPerPage);
-        $series = $repository->findAllByQuery('avengers', 1)->getResults();
-        $this->assertEquals($stubSeries->getData()->getResults(), $series);
+        $this->client->send(Argument::any())->willReturn($jsonResponse);
+        $sut = new SeriesRepository($this->client->reveal());
+        $stubSeries = $sut->getSeries($this->queryMock->reveal());// avengers
+        $repository = new SerieRepository($sut, $this->queryMock->reveal(), $this->comicRepositoryMock->reveal(), $this->comicQueryMock->reveal(), $this->comicsPerPage);
+        $series = $repository->findAllByQuery('avengers', 1);
+        $this->assertEquals($stubSeries->getData()->getResults(), $series->getResults());
       }
 
+      /**
+       * {@inheritdoc}
+       */
+      public function tearDown()
+      {
+        $this->prophet->checkPredictions();
+      }
 }
